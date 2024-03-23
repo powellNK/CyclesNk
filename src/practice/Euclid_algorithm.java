8b212729f88dc03cb5ff35f11491ccf6665c60c9
powellNK <89803160948v@gmail.com>
2024-03-23 22:36:37 +0300

    Euclid_algorithm 2.0

--- src/practice/Euclid_algorithm.java
+++ src/practice/Euclid_algorithm.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Euclid_algorithm {
     public static void main(String[] args){
-        // Алгоритм Евклида для вычисления наибольшего общего делителя двух натуральных чисел,
-        // формулируется так: нужно заменять большее число на разность большего и меньшего до тех пор, пока одно из них не станет равно нулю; тогда второе и есть НОД.
+        // Алгоритм Евклида для вычисления наибольшего общего делителя двух натуральных чисел формулируется так:
+        // нужно заменять большее число на разность большего и меньшего до тех пор, пока одно из них не станет равно нулю; тогда второе и есть НОД.
         Scanner reader = new Scanner(System.in);
         int a = reader.nextInt();
         int b = reader.nextInt();
-        int counterCycle = 0;
+        int copyA = a;
+        int copyB = b;
+        int counterCycle1 = 0;
+        int counterCycle2 = 1;
+        //1 метод
         while (a!=0 && b!=0){
             if (a>b){
                 a-=b;
@@ -17,8 +21,20 @@ public class Euclid_algorithm {
                 b-=a;
             }
             System.out.println("a: "+a+"    b: "+b);
-            counterCycle++;
+            counterCycle1++;
         }
-        System.out.println("НОД:" + a + "   Итераций цикла: "+counterCycle);
+    //Модифицированный алгоритм Евклида для вычисления наибольшего общего делителя двух натуральных чисел, формулируется так:
+    //нужно заменять большее число на остаток от деления большего на меньшее до тех пор, пока остаток не станет равно нулю; тогда второе число и есть НОД.
+        while (copyA%copyB!=0 && copyB%copyA!=0){
+            if (copyA>copyB){
+                copyA%=copyB;
+            }else{
+                copyB%=copyA;
+            }
+            System.out.println("a: "+copyA+"    b: "+copyB);
+            counterCycle2++;
+        }
+        System.out.println("1 метод. НОД:" + a + "   Итераций цикла: "+counterCycle1);
+        System.out.println("2 метод. НОД:" + copyA + "   Итераций цикла: "+counterCycle2);
     }
 }
